e3327332317f8d8b172401c954ca34beafcd44e8
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-11 14:23:40 -0400

    All exercises done for operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
+            int num1 = 12;
+            int num2 = 13;
+            Console.WriteLine(num1+num2);
+            Console.WriteLine(num1-num2);
+            Console.WriteLine(num2/num1);
+            Console.WriteLine(num1%num2);
+
             var a = 17;
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
+            if (a == 17 && b == 4)
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
             Console.WriteLine("Enter the radius of your circle:");
 
@@ -27,6 +35,7 @@
             return area; 
         
         }
+        
 
     }
 }
