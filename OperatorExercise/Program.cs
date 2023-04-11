904a670de8ae28ef0ca4ab12121cecbf8325a1f8
hassan8118 <mujtabahassan1234567@gmail.com>
2023-04-11 14:00:45 -0400

    Ex1,2 Operators done

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,10 +8,25 @@
             var b = 4;
             var quotient = a / b;
             var remainder = a % b;
-
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            Console.WriteLine("Enter the radius of your circle:");
+
+            var userInput = Console.ReadLine();
+
+            
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
 
         }
+
+        public static double AreaOfCircle(double radius)
+        { 
+          var area = Math.PI * Math.Pow (2, radius);
+            return area; 
         
+        }
+
     }
 }
